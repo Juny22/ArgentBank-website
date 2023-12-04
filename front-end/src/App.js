d800e7317e9de64a9d851cc9b580924d4bdd9e71
Lang Ya <lang.ya351@gmail.com>
2023-12-04 19:49:28 +0100

    test deploy

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,11 +8,11 @@ import store from './store/store';
 function App() {
   return (
     <Provider store={store}>
-      <Router basename="/ArgentBank-website">
+      <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<SignIn />} />
-          <Route path="/profile" element={<User />} />
+          <Route path="/ArgentBank-website/" element={<Home />} />
+          <Route path="/ArgentBank-website/login" element={<SignIn />} />
+          <Route path="/ArgentBank-website/profile" element={<User />} />
           <Route path="*" element={<div>Error404</div>} />
         </Routes>
       </Router>
